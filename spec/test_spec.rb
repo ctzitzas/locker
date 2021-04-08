1ceca1d42be1f49ebc27995ec687a45b4d19f959
Chris Tzitzas <ctzitzas@gmail.com>
2021-04-08 15:15:13 +1000

    Completed tests on edit_entry method

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -89,44 +89,113 @@ RSpec.describe Locker do
   
     it 'should add a password to password category' do
       expect(@locker.add_password('gmail', 'chris@gmail.com', 'temporary')).to eq (
-        {
-          'passwords' => [
-            {
-            'name' => 'gmail',
-            'user' => 'chris@gmail.com',
-            'pword' => 'temporary'
-            }
-          ],
-          'servers' => [],
-          'notes' => []
-        })
+        [
+          {
+          'name' => 'gmail',
+          'user' => 'chris@gmail.com',
+          'pword' => 'temporary'
+          }
+        ])
     end
 
     it 'should be able to add a server to server category' do
-      expect(@locker.add_server('local', 'admin', 'temporary', '192.168.1.12')).to eq ({
-        'passwords' => [],
-        'servers' => [
+      expect(@locker.add_server('local', 'admin', 'temporary', '192.168.1.12')).to eq (
+        [
           'name' => 'local',
           'user' => 'admin',
           'pword' => 'temporary',
           'ip_address' => '192.168.1.12',
           'ports' => nil,
           'notes' => nil
-        ],
-        'notes' => []
-      })
+        ])
     end
 
     it 'should be able to add a note to note category' do
-      expect(@locker.add_note('Secret recipes', 'List of herbs and spices')).to eq ({
-        'passwords' => [],
-        'servers' => [],
-        'notes' => [
+      expect(@locker.add_note('Secret recipes', 'List of herbs and spices')).to eq (
+        [
           'name' => 'Secret recipes',
           'note' => 'List of herbs and spices'
-        ]
-      })
+        ])
     end
+  end
+
+  describe 'Editing entries' do
+    before(:each) do
+      @test_data = {
+        'passwords' => [
+          {
+          'name' => 'gmail',
+          'user' => 'chris@gmail.com',
+          'pword' => 'temporary'
+         },
+          { 
+          'name' => 'apple',
+          'user' => 'chris@me.com',
+          'pword' => 'temporary'
+          },
+          {
+          'name' => 'github',
+          'user' => 'chris@me.com',
+          'pword' => 'temporary'
+          }
+        ],
+        'servers' => [
+          {
+          'name' => 'local',
+          'user' => 'admin',
+          'pword' => 'temporary',
+          'IP_address' => '192.168.1.12',
+          'ports' => [
+            '21 - open',
+            '45 - open',
+            '65 - open'
+            ],
+          'notes' => 'This server is currently down'
+          },
+          {
+          'name' => 'work',
+          'user' => 'admin',
+          'pword' => 'temporary',
+          'IP_address' => '192.168.1.12',
+          'ports' => [
+            '21 - open',
+            '45 - open',
+            '65 - open'
+            ],
+          'notes' => 'This server is currently good'
+          }
+        ],
+        'notes' => [
+          {
+            'name' => 'list of secret recipes',
+            'note' => 'Not much here at the moment'
+          },
+          {
+            'name' => 'secret phone numbers',
+            'note' => 'phone numbers for famous people'
+          }
+        ]
+      }
+  
+      @locker = Locker.new(@test_data)
+    end
+
+    it 'should edit name entry for password' do
+      expect(@locker.edit_entry('passwords', 0, 'name', 'google')).to eq 'google'
+    end
+
+    it 'should edit user entry for password' do
+      expect(@locker.edit_entry('passwords', 0, 'user', 'changed')).to eq 'changed'
+    end
+
+    it 'should edit password entry for password' do
+      expect(@locker.edit_entry('passwords', 0, 'pword', 'changed')).to eq 'changed'
+    end
+
+    it 'should edit ip_address entry for server' do
+      expect(@locker.edit_entry('servers', 0, 'ip_address', 'changed')).to eq 'changed'
+    end
+
   end
 end
   
