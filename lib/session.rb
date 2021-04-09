ff69c5725787f2475f24c826504d650a6976cb33
Chris Tzitzas <ctzitzas@gmail.com>
2021-04-09 18:08:00 +1000

    Added locker functions to session Deleted locker class Added all locker class functionality to session class to make coding easier Modified set up function

--- lib/session.rb
+++ lib/session.rb
@@ -5,23 +5,24 @@ require 'base64'
 
 class Session
 
+  attr_accessor :locker
+
   def initialize(name, plain_password, encrypted_data = nil)
     @name = name
     @password = plain_password
     @salt = 'thissalt'
     encrypted_data == nil ? @data = set_up() : @data = JSON.parse(decrypt_it(encrypted_data))
+    write_to_disk()
   end
 
   def set_up
-    empty_data = { 'passwords' => {}, 'servers' => {}, 'notes' => {} }
     system 'mkdir', "../data/#{@name}"
-    write_to_disk(empty_data)
-    empty_data
+    empty_data = { 'passwords' => [], 'servers' => [], 'notes' => [] }
   end
 
-  def write_to_disk(data)
+  def write_to_disk()
     hash = create_hash(@password)
-    enc_data = Base64.encode64(encrypt_it(JSON.generate(data)))
+    enc_data = Base64.encode64(encrypt_it(JSON.generate(@data)))
     File.open("../data/#{@name}/data",'w+') do |f|
       f.write(hash)
     end
@@ -55,7 +56,45 @@ class Session
   end
 
   def get_entry(category, index)
-    @data[category][index].map {|k,value| value}
+    @data[category][index]
+  end
+
+  def display_entry(category, index)
+    display = @data[category][index].map {|key, value| "#{key} - #{value}"}
+  end
+
+  def add_password(name, username, password)
+    @data['passwords'] << {
+      'name' => name,
+      'user' => username,
+      'pword' => password
+    }
+  end
+
+  def add_server(name, user, pword, ip_address, ports = nil , notes = nil)
+    @data['servers'] << {
+      'name' => name,
+      'user' => user,
+      'pword' => pword,
+      'ip_address' => ip_address,
+      'ports' => ports,
+      'notes' => notes
+    }
+  end
+
+  def add_note(name, note)
+    @data['notes'] << {
+      'name' => name,
+      'note' => note
+    }
+  end
+
+  def edit_entry(category, index, entry, new)
+    @data[category][index][entry] = new
+  end
+
+  def delete_entry(category, index)
+    @data[category].delete_at(index)
   end
 
 end
